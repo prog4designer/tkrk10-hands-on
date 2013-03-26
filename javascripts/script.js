0338a06ae6e1a9f1b746f91af451a4ffe59f439c
Tsukasa Obara <obara@saucer.jp>
2013-03-26 21:01:47 +0900

    location.searchからlocation.hashに変更
    hashの仕様を追加

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,25 @@ $(function(){
 
 
   // ***************
+  // hashの仕様
+  // ***************
+  // e.g. index.html#ec=fff,111,000&ec=fff&lg=fff&h=fff&hd=fff&ct=fff
+
+  // ec = extend color code / 拡張で読み込まれるカラーコード。,区切りで複数可能
+  // lg = logo color / 現在のlogo色。状態保存用。複数不可。
+  // h = heading color / 現在のh1...h6の色。状態保存用。複数不可。
+  // hd = header bg color / 現在のヘッダーの背景色。状態保存用。複数不可。
+  // ct = contents bg color / 現在のサイトの背景色。状態保存用。複数不可。
+  var HASHES_NAME = {
+    EXTEND: 'ec',
+    LOGO: 'lg',
+    HEADING: 'h',
+    HEADER: 'hd',
+    CONTENTS: 'ct'
+  };
+
+
+  // ***************
   // init
   // ***************
 
@@ -60,40 +79,42 @@ $(function(){
     var listsClass = 'color-sample';
 
     // default colors
-    var _defaultColors = {
-      1:  'E1DBC9',
-      2:  'C5BFB0',
-      3:  '746643',
-      4:  '3F3724',
-      5:  '3F5466',
-      6:  '062737',
-      7:  '1586BF',
-      8:  'FFF1C6',
-      9:  'F1BD3D',
-      10: 'B68299',
-      11: '982365',
-      12: 'E0EC8F',
-      13: '61B816'
-    };
+    var _defaultColors = [
+      'E1DBC9',
+      'C5BFB0',
+      '746643',
+      '3F3724',
+      '3F5466',
+      '062737',
+      '1586BF',
+      'FFF1C6',
+      'F1BD3D',
+      'B68299',
+      '982365',
+      'E0EC8F',
+      '61B816'
+    ];
 
     // get extend colors
     this._get = function(){
       var colors = null;
-      if( location.search == '' ){
+      if( location.hash == '' ){
         colors = _defaultColors;
       } else {
-        // get URL query
-        colors = getArgs();
+        // get URL hashes
+        var hashes = getHashes();
+        var extendColors = HASHES_NAME.EXTEND;
+        colors = hashes[ extendColors ].split(',');
       }
       return colors;
     };
 
     // create color list
-    this._create = function( obj ){
-      var colors = obj;
+    this._create = function( ary ){
+      var colors = ary;
       var $lists = $('<ul/>').addClass( listsClass );
-      $.each( colors, function(key, val){
-        var code = adjustColorCode( val );
+      $.each( colors, function( i ){
+        var code = adjustColorCode( colors[ i ] );
         var $list = $('<li/>');
         $list.css('background-color', code);
         $list.text( code );
@@ -132,6 +153,24 @@ $(function(){
       value = decodeURIComponent( value );
       args[ argname ] = value;
     }
+    return args;
+  }
+
+
+  // getHashes
+  function getHashes() {
+    var args = new Object();
+    var hash = location.hash.substring(1);
+    var pairs = hash.split('&');
+    for( var i = 0; i < pairs.length; i++ ) {
+      var pos = pairs[i].indexOf('=');
+      if( pos == -1 ) continue;
+      var argname = pairs[i].substring( 0, pos );
+      var value= pairs[i].substring( pos + 1 );
+      value = decodeURIComponent( value );
+      args[ argname ] = value;
+    }
+    console.log( args );
     return args;
   }
 
